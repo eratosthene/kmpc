8aae32f19d9bf1044b52794e80b7fb26f40ec491
Chris Graham <chris.graham@blackboard.com>
2018-01-19 10:18:42 -0600

    documenting configpanel.py

--- configpanel.py
+++ configpanel.py
@@ -19,61 +19,76 @@ from Queue import Queue, Empty
 from functools import partial
 
 class ConfigTabbedPanelItem(TabbedPanelItem):
+    """The Config tab, for misc. mpd configs and internal functions."""
 
     def handle_mpd_error(self,result):
+        """Callback for handling mpd exceptions."""
         Logger.error('Config: MPDIdleHandler Callback error: {}'.format(result))
 
     def update_mpd_status(self,result):
+        """Callback for mpd status updates."""
         Logger.debug('Config: update_mpd_status')
+        # set up the crossfade slider
         if 'xfade' in result:
             v = int(result['xfade'])
         else:
             v = 0
         self.ids.crossfade_slider.value=v
+        # set up the mixrampdb slider
         if 'mixrampdb' in result:
             v = round(float(result['mixrampdb']),6)
         else:
             v = 0.0
         self.ids.mixrampdb_slider.value=float(str(v)[1:])
+        # set up the mixrampdelay slider
         if 'mixrampdelay' in result:
             v = round(float(result['mixrampdelay']),6)
         else:
             v = 0.0
         self.ids.mixrampdelay_slider.value=v
+        # get the host's IP address and display it
         self.ids.ip_label.text="IP Address: "+format(self.get_ip())
 
     def printit(self,result):
+        """An internal debugging function. Probably shouldn't ever be used."""
         print format(result)
 
     def change_crossfade(self):
+        """Callback when user changes crossfade slider."""
         Logger.info('Config: change_crossfade')
         v=int(round(self.ids.crossfade_slider.value))
         self.protocol.crossfade(str(v)).addErrback(self.handle_mpd_error)
 
     def change_mixrampdb(self):
+        """Callback when user changes mixrampdb slider."""
         Logger.info('Config: change_mixrampdb')
         v=0.0-round(self.ids.mixrampdb_slider.value,6)
         self.protocol.mixrampdb(str(v)).addErrback(self.handle_mpd_error)
 
     def change_mixrampdelay(self):
+        """Callback when user changes mixrampdelay slider."""
         Logger.info('Config: change_mixrampdelay')
         v=round(self.ids.mixrampdelay_slider.value,6)
         self.protocol.mixrampdelay(str(v)).addErrback(self.handle_mpd_error)
 
     def git_pull(self):
+        """Method that runs a git pull on the current folder."""
         Logger.info('Config: git_pull')
         g = git.cmd.Git(os.getcwd())
         Logger.info(g.pull())
 
     def do_reboot(self):
+        """Method that reboots the host."""
         Logger.info('Config: reboot')
         call('reboot')
 
     def do_poweroff(self):
+        """Method that shuts down the host."""
         Logger.info('Config: poweroff')
         call('poweroff')
 
     def get_ip(self):
+        """Method that tries to get the local IP address, and returns localhost if there isn't one."""
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         try:
             # doesn't even have to be reachable
@@ -86,16 +101,20 @@ class ConfigTabbedPanelItem(TabbedPanelItem):
         return IP
 
     def enqueue_output(self,out,queue,event,popup,tpath,synchost,layout,sv):
+        """Method that is called in its own thread to write commandline script output to a popup, then clean up afterwards."""
+        # loop through the stdout output and shove it into the queue
         for line in iter(out.readline, b''):
             queue.put(line)
+        # close the output stream, cancel the .1 second update event, dismiss the popup
         out.close()
         event.cancel()
-        popup.dismiss()
+        popup.dismiss() # seems like this is too early, since we're writing more data in the next few lines
         Logger.info('Filesync: Cleaning up')
         l=Label(text='Cleaning up temporary files',size_hint=(None,None),font_size='12sp',halign='left')
         l.bind(texture_size=l.setter('size'))
         layout.add_widget(l)
         sv.scroll_to(l)
+        # try removing all temporary files, should probably catch more specific exceptions in case something truly breaks
         try:
             call(['sudo','cp',tpath,'/var/lib/mpd/playlists/root.m3u'])
             os.remove(tpath)
@@ -108,17 +127,21 @@ class ConfigTabbedPanelItem(TabbedPanelItem):
             pass
 
     def write_queue_line(self,q,layout,sv,dt):
+        """Method that checks the queue for output and adds a new label line if any output exists."""
         try: line=q.get_nowait()
         except Empty:
             pass
         else:
+            # TODO: figure out why lines all seem to have a blank line between them, probably something to do with \n at the end or something
             l=Label(text=line,size_hint=(None,None),font_size='12sp',halign='left')
             l.bind(texture_size=l.setter('size'))
             layout.add_widget(l)
             sv.scroll_to(l)
 
     def filesync(self):
+        """Callback when the user presses the Sync button. This relies on keyless ssh working, and does most of the work in shell scripts."""
         Logger.info('Config: filesync')
+        # set up a popup containing a scrollview to contain stdout output
         layout = GridLayout(cols=1, spacing=1, padding=1, size_hint_y=None)
         layout.bind(minimum_height=layout.setter('height'))
         popup = Popup(title='Sync',size_hint=(0.8,1))
@@ -126,27 +149,39 @@ class ConfigTabbedPanelItem(TabbedPanelItem):
         sv.add_widget(layout)
         popup = Popup(title='Sync',content=sv,size_hint=(0.8,1))
         popup.open()
+        # temp file for the rsync, probably a better way to do this
+        # this file describes exactly what songs should exist on the host, no more, no less
         tpath="/tmp/rsync.inc"
+        # look in the ini file for all the relevant paths
         synchost = App.get_running_app().root.config.get('mpd','synchost')
         syncbasepath = App.get_running_app().root.config.get('mpd','syncbasepath')
         syncfanartpath= App.get_running_app().root.config.get('mpd','syncfanartpath')
         basepath = App.get_running_app().root.config.get('mpd','basepath')
         fanartpath= App.get_running_app().root.config.get('mpd','fanartpath')
         Logger.info('Filesync: Copying rsync file to carpi')
+        # TODO: figure out why this doesn't show up on the screen until after the os.walk has completed
         l=Label(text='Copying rsync file to carpi',size_hint=(None,None),font_size='12sp',halign='left')
         l.bind(texture_size=l.setter('size'))
         layout.add_widget(l)
         sv.scroll_to(l)
+        # copy the rsync file from the synchost
+        # TODO: implement this using python instead of call
         call(["scp",synchost+":rsync.inc",tpath])
         filelist={}
+        # use codecs.open to ensure the file is read as utf8, otherwise special chars can be mangled
         with codecs.open(tpath,'r','utf-8') as f:
             for line in f:
+                # add each filename to a dict for easy searching later
                 filelist[line.rstrip().encode(encoding='UTF-8')]=True
         Logger.info('Filesync: Removing old files from carpi')
+        # TODO: figure out why this doesn't show up on the screen until after the os.walk has completed
         l=Label(text='Removing old files from carpi',size_hint=(None,None),font_size='12sp',halign='left')
         l.bind(texture_size=l.setter('size'))
         layout.add_widget(l)
         sv.scroll_to(l)
+        # this whole block walks the filesystem and deletes any file that is not in the rsync file
+        # the sync operation is split up into a delete and a copy because that was the only way I could get
+        # rsync to work correctly, it was always copying/deleting the wrong things otherwise
         for dirpath, dirnames, filenames in os.walk(basepath):
             if len(filenames)>0:
                 rpath = dirpath[len(basepath+os.sep):].encode(encoding='UTF-8')
@@ -156,30 +191,49 @@ class ConfigTabbedPanelItem(TabbedPanelItem):
                     if fpath not in filelist:
                         Logger.debug("Filesync: Deleting "+apath)
                         os.remove(apath)
+        # build a shell script. should probably use a real tempfile
+        # TODO: somehow do this all in python instead of shell script, it's ugly
         with open('sync.sh','w') as sfile:
             sfile.write("#!/bin/bash\n")
+            # delete all empty directories
             sfile.write('find "'+basepath+'" -type d -empty -delete 2>/dev/null\n')
+            # copy/update only the files that exist in the rsync file
             sfile.write('rsync -vruxhm --files-from="'+tpath+'" '+synchost+':"'+syncbasepath+'"/ "'+basepath+'"\n')
+            # copy the car sticker database to the synchost
             sfile.write('scp /var/lib/mpd/sticker.sql '+synchost+':/tmp\n')
+            # build a secondary shell script to perform the sticker update on the synchost
             with open('/tmp/scmd','w') as f:
+                # attach the copied database and merge sticker data from it to update ratings user has added in the car
                 f.write("attach database \"/tmp/sticker.sql\" as carpi;\n")
                 f.write("replace into sticker select * from carpi.sticker;\n")
                 f.write("replace into carpi.sticker select * from sticker where name='rating';\n")
                 f.write(".quit\n")
+            # copy secondary script to synchost and run it
             sfile.write('scp /tmp/scmd '+synchost+':/tmp\n')
             sfile.write('ssh -t '+synchost+' sudo sqlite3 /var/lib/mpd/sticker.sql < /tmp/scmd\n')
+            # copy the now updated sticker database back from the synchost
             sfile.write('scp '+synchost+':/tmp/sticker.sql /tmp\n')
+            # build a secondary shell script to perform the sticker update on the host
             with open('/tmp/scmd','w') as f:
+                # attach the copied database and merge sticker data from it to update ratings user has added at home
                 f.write("attach database \"/tmp/sticker.sql\" as carpi;\n")
                 f.write("replace into sticker select * from carpi.sticker;\n")
                 f.write(".quit\n")
+            # run the secondary script
             sfile.write('cat /tmp/scmd | sudo sqlite3 /var/lib/mpd/sticker.sql\n')
+            # rsync over all the fanart
             sfile.write('rsync -vruxhm '+synchost+':"'+syncfanartpath+'"/ "'+fanartpath+'"\n')
+            # tell mpd about any changes
             sfile.write("mpc update\n")
+        # make the shell script executable
         os.chmod('sync.sh',os.stat('sync.sh').st_mode|0111)
+        # queue for holding stdout
         q = Queue()
+        # create a subprocess for the shell script and capture stdout
         p = Popen(['./sync.sh'],stdout=PIPE,bufsize=1,close_fds=True)
+        # check the stdout queue every .1 seconds and write lines to the scrollview if there is output
         event=Clock.schedule_interval(partial(self.write_queue_line,q,layout,sv),0.1)
+        # run all this crap in a separate thread to be non-blocking
         t = Thread(target=self.enqueue_output,args=(p.stdout,q,event,popup,tpath,synchost,layout,sv))
         t.daemon = True
         t.start()
