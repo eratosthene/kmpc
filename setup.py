73d0b6ed74a9a2f1aff2bdaf8c736e28486fd5e9
Chris Graham <chris.graham@blackboard.com>
2018-01-20 21:49:14 -0600

    fixing up versions

--- setup.py
+++ setup.py
@@ -1,25 +1,8 @@
 from setuptools import setup
 
-import kmpc.version
-
-VERSION = ".".join(map(str, kmpc.VERSION))
-
-LICENSE = """\
-Copyright (C) 2017-2018  Chris Graham <eratosthene@gmail.com>
-
-kmpc is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-kmpc is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.  You should have received a copy of the GNU Lesser General Public License
-along with kmpc.  If not, see <http://www.gnu.org/licenses/>.\
-"""
+from kmpc.version import VERSION,VERSION_STR
 
 setup(
-    version=VERSION,
+    version=VERSION_STR,
     setup_cfg=True,
 )
