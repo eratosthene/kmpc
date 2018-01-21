b465b6d5e53106dd821462e38c34110a92e945bf
Chris Graham <chris.graham@blackboard.com>
2018-01-21 17:30:23 -0600

    adding command line handling, -q and -d options

--- kmpc/kmpcmanager.py
+++ kmpc/kmpcmanager.py
@@ -11,7 +11,6 @@ import json
 import subprocess
 import tempfile
 import shutil
-import ConfigParser
 from pkg_resources import resource_filename
 
 # make sure we are on an updated version of kivy
@@ -20,25 +19,11 @@ kivy.require('1.10.0')
 
 #install twisted reactor to interface with mpd
 from kivy.support import install_twisted_reactor
-# this try/catch block is specifically because sphinx docs fail otherwise
-try:
-    install_twisted_reactor()
-except AttributeError:
-    pass
+install_twisted_reactor()
 from twisted.internet import reactor, protocol, task, defer, threads
 
-# import config and set key values before other imports
-from kivy.config import Config
-# this try/catch block is specifically because sphinx docs fail otherwise
-try:
-    Config.set('kivy','log_level','info') # set this to 'debug' to see more verbose debug messages
-    Config.set('graphics','width',1280)
-    Config.set('graphics','height',720)
-    Config.set('kivy','keyboard_mode','system')
-except AttributeError:
-    pass
-
 # import all the other kivy stuff
+from kivy.config import Config
 from kivy.app import App
 from kivy.support import install_twisted_reactor
 from kivy.config import Config
@@ -332,6 +317,13 @@ class ManagerInterface(TabbedPanel):
 
 
 class ManagerApp(App):
+
+    def __init__(self,args):
+        Config.set('graphics','width',1280)
+        Config.set('graphics','height',720)
+        Config.set('kivy','keyboard_mode','system')
+        super(self.__class__,self).__init__()
+
     def build(self):
         config=Helpers.loadconfigfile()
         # setup some variables that interface.kv will use
