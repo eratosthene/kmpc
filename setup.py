9819d75279c16a0ad86aee17c11a2eb7f691395e
Chris Graham <chris.graham@blackboard.com>
2018-01-20 21:54:02 -0600

    adding download_url to setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ from kmpc.version import VERSION,VERSION_STR
 
 setup(
     version=VERSION_STR,
+    download_url = 'https://github.com/eratosthene/kmpc/archive/'+VERSION_STR+'.tar.gz',
     setup_cfg=True,
 )
