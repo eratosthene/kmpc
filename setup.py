d6dee291800bf7e17cc8c897954d7c9fc2ce3cd2
Chris Graham <chris.graham@blackboard.com>
2018-01-26 12:48:21 -0600

    setting latest version of setuptools as required. not sure if this actually helps anything

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ from kmpc.version import VERSION,VERSION_STR
 setup(
     version=VERSION_STR,
     download_url = 'https://github.com/eratosthene/kmpc/archive/'+VERSION_STR+'.tar.gz',
+    setup_requires = ['setuptools>=38.4.0'],
     setup_cfg=True,
 )
